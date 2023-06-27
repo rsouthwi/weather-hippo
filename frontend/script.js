2630952b7debedee2a727afed48e6cf5bcb8e579
Ron Southwick <ronlsouthwick@gmail.com>
2023-06-27 17:49:30 -0400

    add function to hit weather api and display the goods

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,52 @@
 let hippoStatus = "in";
 
+const getWeatherData = (location) => {
+    const url = "https://www.ronsouthwick.com/weather-api";
+    const payload = {"location": location};
+    const headers = {"Content-type": "application/json", "host": "ronsouthwick.com"};
+
+    fetch(url, {method: 'POST', body: JSON.stringify(payload), headers: headers})
+        .then(response => {
+            if (response.status === 400) throw Error("I couldn't find");
+            else if (!response.ok) throw Error("I think something's wrong. Sorry.");
+            return response.json()
+        })
+        .then(data => populateSpeechBubble(data))
+        .catch(error => {
+            if (!error.message.includes("couldn't find")) location = "";
+            populateSpeechBubble( {error: error.message, location: location} );
+        });
+    toggleHippo();
+}
+
+const setWeatherImage = (condition = null) => {
+    const container = document.querySelector("#container");
+    if (condition)  container.style.backgroundImage = `url("images/${condition}.gif")`;
+    else container.style.background = "";
+}
+
+const populateSpeechBubble = (payload) => {
+    const speechBubble = document.getElementById("bubble");
+    const {condition, location, temperature, error} = payload;
+    let message = `<p>Hmmm... ${error} ${location}</p>`;
+    if (!error) {
+        message = `<p>The weather in ${location} is ${condition}!</p>\n<p>It's ${temperature}&deg;F out here!</p>`;
+        setWeatherImage(condition);
+    }
+    speechBubble.innerHTML = message;
+    setTimeout(()=> speechBubble.classList.add("load"), 5000);
+}
+
 const toggleHippo = () => {
     if (hippoStatus === "moving") return;
     hippoStatus = "moving";
     const house = document.getElementById("house");
-    const speechBubble = document.getElementById("bubble");
+
     if (!house.src.includes("_open")) {
         house.src = "images/house_open.gif";
-        speechBubble.innerHTML = "<p>The weather in Alexandria is smokey!</p>\n<p>It's 75&deg;F out here!</p>"
-        setTimeout(()=> speechBubble.classList.add("load"), 5000);
         setTimeout(()=> hippoStatus = "out", 5000);
     } else {
+        const speechBubble = document.getElementById("bubble");
         house.src = "images/house_close.gif";
         speechBubble.classList.remove("load");
         setTimeout(()=> hippoStatus = "in", 5000);
@@ -23,16 +59,23 @@ const hippoGoesIn = () => {
 };
 
 const hippoComesOut = () => {
-    if (hippoStatus === "in") toggleHippo();
-    else if (hippoStatus === "moving") setTimeout(hippoComesOut, 1000);
+    if (hippoStatus === "in") {
+        const locationInput = document.querySelector("#loc");
+        const location = locationInput.value;
+        locationInput.blur();
+        getWeatherData(location);
+    } else if (hippoStatus === "moving") setTimeout(hippoComesOut, 1000);
 }
 
 const registerEvents = (event) => {
-    const findOutButton = document.querySelector("#findOut");
-    findOutButton.addEventListener("click", hippoComesOut);
-
     const locationInput = document.querySelector("#loc");
     locationInput.addEventListener("focus", hippoGoesIn);
+
+    const form = document.querySelector('form');
+    form.addEventListener("submit", (event => {
+        event.preventDefault();
+        hippoComesOut();
+    }))
 };
 
 window.addEventListener("DOMContentLoaded", registerEvents);
